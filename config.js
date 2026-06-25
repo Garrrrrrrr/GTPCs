7d976633b698e685037e0a8b8e7cc3a4c0e7d559
Garr-ai <g.tse8888@gmail.com>
2026-06-25 04:19:00 -0600

    fix config

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.CONFIG = {
   businessName: "GTPCS",
   tagline: "High-end GPUs, gaming PCs, and PC parts in Edmonton, Alberta.",
   inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/1KzVm-sNSR8SI-3_tcHJSE9bxWvNKSWcfBymlG6Ti6L8/gviz/tq?tqx=out:csv&gid=190180623",
-  appsScriptWebAppUrl: "https://script.google.com/macros/s/AKfycby5IbP8jRdg8f25BDRfp47gHBZHhV06BPyqktqjg_b57xpFKsQ5jtECEdT3CiiyDmTxJg/exec",
+  appsScriptWebAppUrl: "https://script.google.com/macros/s/AKfycbzJWqsrZf9GL6UQffI1UzG5ZhgYFIlAVyT06ghNLYDhSMs78rbcccbhAat97oF-Bi86fA/exec",
   requestFormToken: "gtpcs-public-request-v1",
   requestPageUrl: "/request/",
   contactEmail: "gtpcca@gmail.com",
