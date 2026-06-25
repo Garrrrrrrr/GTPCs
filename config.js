0ae13dfa2fd8e5982c5c607e915d14dde37fa8b9
Your Name <gtse1@ualberta.ca>
2026-06-25 01:29:06 -0600

    Expose inventory config globally

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const CONFIG = {
+window.CONFIG = {
   businessName: "GTPCS",
   tagline: "High-end GPUs, gaming PCs, and PC parts in Edmonton, Alberta.",
   inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/1KzVm-sNSR8SI-3_tcHJSE9bxWvNKSWcfBymlG6Ti6L8/gviz/tq?tqx=out:csv&gid=190180623",
