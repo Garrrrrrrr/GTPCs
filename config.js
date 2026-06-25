012a5a7d76881daf631906618b17cb3450f3edad
Garr-ai <g.tse8888@gmail.com>
2026-06-25 03:58:46 -0600

    Add webapp script for google form

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ window.CONFIG = {
   businessName: "GTPCS",
   tagline: "High-end GPUs, gaming PCs, and PC parts in Edmonton, Alberta.",
   inventoryCsvUrl: "https://docs.google.com/spreadsheets/d/1KzVm-sNSR8SI-3_tcHJSE9bxWvNKSWcfBymlG6Ti6L8/gviz/tq?tqx=out:csv&gid=190180623",
-  appsScriptWebAppUrl: "PASTE_DEPLOYED_APPS_SCRIPT_WEB_APP_URL_HERE",
+  appsScriptWebAppUrl: "https://script.google.com/macros/s/AKfycbyTElZKKQ_VjwTzegc3uBi2Spi8BXStm97eib6S9QH-Ce7D6XsCMDELLX3C4vlx32x1Ug/exec",
   requestPageUrl: "/request/",
   contactEmail: "gtpcca@gmail.com",
   location: "Edmonton, Alberta"
